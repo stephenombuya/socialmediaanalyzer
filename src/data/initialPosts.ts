f4b80eb7da95a16752a0e50656a9bd5cf1ba0137
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2024-12-12 17:08:14 +0300

    Create initialPosts.ts

---
+++ src/data/initialPosts.ts
@@ -0,0 +1,25 @@
+import { Post } from '../types/Post';
+
+export const initialPosts: Post[] = [
+  {
+    postId: '1',
+    content: 'Hello world!',
+    likes: 10,
+    comments: 5,
+    shares: 3,
+  },
+  {
+    postId: '2',
+    content: 'Check out this amazing photo!',
+    likes: 20,
+    comments: 8,
+    shares: 2,
+  },
+  {
+    postId: '3',
+    content: 'Exciting news!',
+    likes: 15,
+    comments: 10,
+    shares: 4,
+  },
+];
